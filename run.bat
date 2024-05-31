bad8e4a5c8c433888715ca89befc37557bea8010
Buglot <65818720+buglot@users.noreply.github.com>
2024-05-31 22:37:33 +0700

    run.bat

--- run.bat
+++ run.bat
@@ -1,7 +1,27 @@
 @ECHO OFF
-echo new venv
-python -m venv .env
-\env\Scripts\activate
+if not defined PYTHON (set PYTHON=python)
+if not defined VENV_DIR (set "VENV_DIR=%~dp0%env")
+
+
+If Not Exist "%VENV_DIR%\Scripts\activate.bat" (
+    for /f %%i in ('CALL %PYTHON% -c "import sys; print(sys.executable)"') do set FULL_PATH="%%i"
+
+    echo Creating virtual environment
+    !FULL_PATH! -m venv "%VENV_DIR%"
+    echo:
+)
+
+echo Attempting to start venv
+Call "%VENV_DIR%\Scripts\activate.bat"
+echo:
+
+echo Updating Pip
+python -m pip install --upgrade pip
+echo:
+
+echo Installing / 
 pip install -r requirement.txt
+echo:
+
 python app.py
-PAUSE
+pause
